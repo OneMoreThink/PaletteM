3c182b1ff3c2b988ef9403ee5e60eafa885a7041
fine <92whdtjs@gmail.com>
2024-09-11 11:23:17 +0900

    chore: 프로젝트 시작

---
+++ PaletteM/PaletteM/PaletteMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletteMApp.swift
+//  PaletteM
+//
+//  Created by 이종선 on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PaletteMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
