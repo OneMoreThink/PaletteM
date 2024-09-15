83505aa92bc338925e09e8b6014ae74635c5c145
fine <92whdtjs@gmail.com>
2024-09-15 17:29:12 +0900

    [feat] #9 - Color Codable 확장

--- PaletteM/PaletteM/Model/Helper/Color+Extensions.swift
+++ PaletteM/PaletteM/Model/Helper/Color+Extensions.swift
@@ -13,3 +13,31 @@ extension Color {
         self.init(uiColor)
     }
 }
+
+extension Color: Codable {
+    enum CodingKeys: String, CodingKey {
+        case red, green, blue, opacity
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var opacity: CGFloat = 0
+        UIColor(self).getRed(&red, green: &green, blue: &blue, alpha: &opacity)
+        try container.encode(red, forKey: .red)
+        try container.encode(green, forKey: .green)
+        try container.encode(blue, forKey: .blue)
+        try container.encode(opacity, forKey: .opacity)
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let red = try container.decode(CGFloat.self, forKey: .red)
+        let green = try container.decode(CGFloat.self, forKey: .green)
+        let blue = try container.decode(CGFloat.self, forKey: .blue)
+        let opacity = try container.decode(CGFloat.self, forKey: .opacity)
+        self.init(red: Double(red), green: Double(green), blue: Double(blue), opacity: Double(opacity))
+    }
+}
