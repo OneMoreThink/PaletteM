4d7d11d45b5295392b005ed66ec393883faf748f
fine <92whdtjs@gmail.com>
2024-09-15 10:38:07 +0900

    [feat] #5 - 색상 추출 로직을 위한 Extension

---
+++ PaletteM/PaletteM/Model/Helper/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  PaletteM
+//
+//  Created by 이종선 on 9/15/24.
+//
+
+import SwiftUI
+
+extension Color {
+    /// Color를 UIColor로 변환
+    init(_ uiColor: UIColor) {
+        self.init(uiColor)
+    }
+}
